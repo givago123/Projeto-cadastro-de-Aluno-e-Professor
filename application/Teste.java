0fcea38f7539baaecf64027041d95f1c4d82083d
Givago Tavares <givago.mubarak1@gmail.com>
2026-02-04 04:26:58 -0300

    Tratamento de exceções

--- application/Teste.java
+++ application/Teste.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -17,54 +18,62 @@ public class Teste {
 		List<Professor> professores = new ArrayList<Professor>();
 		int opcao;
 		
-		do {
-			System.out.print("Digite a opção para cadastro(opção 1- Aluno/ opção 2- Professor/ opção 3 - Sair): ");
-			opcao = Integer.parseInt(sc.next());
-			
-			switch (opcao) {
-			case 1: 
-				System.out.print("Digite o nome do Aluno: ");
-				sc.nextLine();
-				String nome = sc.nextLine();
-				System.out.print("Digite o sobrenome do Aluno: ");
-				String sobrenome = sc.nextLine();
-				System.out.print("Digite a matricula: ");
-				int matricula = sc.nextInt();
-				System.out.println();
-				
-				alunos.add(new Aluno(nome, sobrenome, matricula));
-				
-				System.out.println(alunos.toString());
-				System.out.println();
-				
-				break;
-			case 2:
-				System.out.print("Digite o nome do Professor: ");
-				sc.nextLine();
-				String nomeProfessor = sc.nextLine();
-				System.out.print("Digite o sobrenome do Professor: ");
-				String sobrenomeProfessor = sc.nextLine();
-				System.out.print("Digite a disciplina: ");
-				String disciplina = sc.nextLine();
-				System.out.println();
-				
-				professores.add(new Professor(nomeProfessor, sobrenomeProfessor, disciplina));
-				
-				System.out.println(professores.toString());
-				System.out.println();
-				
-				break;
-			case 3:
-				System.out.println("Saindo...");
-				break;
-			
-			default:
-				throw new IllegalArgumentException("Opção invalida: ");
-			}
-			
-		}while(opcao != 3);
+		try {
 
-		
+			do {
+				System.out.print("Digite a opção para cadastro(opção 1- Aluno/ opção 2- Professor/ opção 3 - Sair): ");
+				opcao = Integer.parseInt(sc.next());
+				
+				switch (opcao) {
+				case 1: 
+					System.out.print("Digite o nome do Aluno: ");
+					sc.nextLine();
+					String nome = sc.nextLine();
+					System.out.print("Digite o sobrenome do Aluno: ");
+					String sobrenome = sc.nextLine();
+					System.out.print("Digite a matricula: ");
+					int matricula = sc.nextInt();
+					System.out.println();
+					
+					alunos.add(new Aluno(nome, sobrenome, matricula));
+					
+					System.out.println(alunos.toString());
+					System.out.println();
+					
+					break;
+				case 2:
+					System.out.print("Digite o nome do Professor: ");
+					sc.nextLine();
+					String nomeProfessor = sc.nextLine();
+					System.out.print("Digite o sobrenome do Professor: ");
+					String sobrenomeProfessor = sc.nextLine();
+					System.out.print("Digite a disciplina: ");
+					String disciplina = sc.nextLine();
+					System.out.println();
+					
+					professores.add(new Professor(nomeProfessor, sobrenomeProfessor, disciplina));
+					
+					System.out.println(professores.toString());
+					System.out.println();
+					
+					break;
+				case 3:
+					System.out.println("Saindo...");
+					break;
+				
+				default:
+					throw new IllegalArgumentException("Opção invalida: ");
+				}
+				
+			}while(opcao != 3);
+		}
+		catch (NumberFormatException e) {
+			System.out.println("Digite um numero valido...");	
+		}
+		catch (InputMismatchException e) {
+			System.out.println("Digite um numero valido...");
+		}
+			
 		sc.close();
 	}
 
